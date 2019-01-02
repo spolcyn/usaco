6b48db063e0eb48b9baf831967e4a51ce3149aab
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-01-02 13:52:07 -0500

    Added data structure and file IO

--- milkingcows/milkingcows.cpp
+++ milkingcows/milkingcows.cpp
@@ -5,10 +5,21 @@ LANG: C++
 */
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    int numberOfFarmers;
+    /* Start/stop times of farmers. 
+     * Farmer N's start in [2N], stop in [2N+1]
+     */
+    int* farmerTimes; // start/stop times of farmers. 
+
+    ofstream input;
+    input.open("milk2.in");
+    input.close();
+
 	return 0;
 }
