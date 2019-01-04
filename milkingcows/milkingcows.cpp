c5d13896843fc5e8fc63d1b2451834109c1b5440
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-01-04 13:33:23 -0500

    Switched to extraction operator

--- milkingcows/milkingcows.cpp
+++ milkingcows/milkingcows.cpp
@@ -6,6 +6,7 @@ LANG: C++
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main()
 {
@@ -14,14 +15,20 @@ int main()
      * Farmer N's start in [2N], stop in [2N+1]
      */
     int* farmerTimes; // start/stop times of farmers. 
-    int bufferSize = 5;
+    int bufferSize = 10;
     char buffer[bufferSize];
 
     std::ifstream input;
     input.open("milk2.in");
 
-    input.get(buffer, bufferSize);
-    std::cout << "Number of farmers: " << buffer << std::endl;
+/*    input.get(buffer, bufferSize);
+    numberOfFarmers = std::stoi(buffer, nullptr, 10); */
+    input >> numberOfFarmers;
+
+//    while(input.get(buffer, bufferSize))
+
+
+    std::cout << "Number of farmers: " << numberOfFarmers << std::endl;
 
     input.close();
 
