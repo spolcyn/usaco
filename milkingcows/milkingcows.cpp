a65dc9b69752f69c32febd865644f1e902e7bc13
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-01-09 11:40:34 -0500

    Cleaned up a bit

--- milkingcows/milkingcows.cpp
+++ milkingcows/milkingcows.cpp
@@ -14,6 +14,7 @@ struct Farmer {
     int startTime, endTime;
 };
 
+/* Comparison methods for sort */
 bool compareFarmerStart(Farmer f1, Farmer f2)
 {
     return f1.startTime < f2.startTime;
@@ -39,9 +40,7 @@ int getFarmerEnd(int farmerNumber, std::vector<Farmer> *farmers) {
 int main()
 {
     int numberOfFarmers;
-    /* Start/stop times of farmers. 
-     * Farmer N's start in [2N], stop in [2N+1]
-     */
+    /* Start/stop times of farmers. */
     std::vector<Farmer> *farmers;
 
     std::ifstream input;
@@ -60,58 +59,46 @@ int main()
 
     input.close();
 
-    /* Compute longest start time */
+    /* Compute milking & idle times */
     std::sort(farmers->begin(), farmers->end(), compareFarmerStart);
 
     int currentStart, currentEnd;
-    int longestMilk, longestDrought;
+    int longestMilk, longestIdle;
 
     currentStart = getFarmerStart(0, farmers);
     currentEnd = getFarmerEnd(0, farmers);
 
     longestMilk = currentEnd - currentStart;
-    longestDrought = 0;
+    longestIdle = 0;
 
     for(Farmer f : *farmers)
     {
-        // if the next farmer starts before the previous ends, extend the continuous milking
+        /* if the next farmer starts before the previous ends, extend the continuous milking */
         if(f.startTime <= currentEnd)
         {
             if(f.endTime > currentEnd)
                 currentEnd = f.endTime;
         }
-        // otherwise, compare current milking and drought to previous records
         else
         {
+            /* compare current milking and idle to previous records */
             if(currentEnd - currentStart > longestMilk)
                 longestMilk = currentEnd - currentStart;
-            if(f.startTime - currentEnd > longestDrought)
-                longestDrought = f.startTime - currentEnd;
+            if(f.startTime - currentEnd > longestIdle)
+                longestIdle = f.startTime - currentEnd;
+            
+            /* Reset chain after breakage */
             currentStart = f.startTime;
             currentEnd = f.endTime;
         }
     }
 
     /* Output results */
-
     std::ofstream output;
+
     output.open("milk2.out");
-
-    output << longestMilk << " " << longestDrought << std::endl;
-
+    output << longestMilk << " " << longestIdle << std::endl;
     output.close();
-
-   /* Test Data Read Code
-    std::cout << "Number of farmers: " << numberOfFarmers << std::endl;
-
-    for(int i = 0; i < numberOfFarmers; i++)
-    {
-        std::cout << getFarmerStart(i, farmers) << ' ';
-        std::cout << getFarmerEnd(i, farmers);
-        std::cout << std::endl;
-    }
-    */
-
 
 	return 0;
 }
