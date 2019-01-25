8d8036aa2c23cd04e546f55dc5d87437655ddfa7
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-01-24 23:06:55 -0500

    Put overloaded operator in class, tested correctness

--- transformations/transform.cpp
+++ transformations/transform.cpp
@@ -7,97 +7,98 @@
 /* Represents a square with a grid inside */
 class Square {
 
-public:
-    
-    /* Returns true if s1 and s2 have the same sidelength, false otherwise */
-    static bool verifySameDimensions(Square* s1, Square* s2)
-    {
-        return s1->getLength() == s2->getLength();
-    }
+    public:
 
-    /* Returns a square equal to initial rotated 90 degrees clockwise */
-    static Square* rotate90DegCW(Square* initial)
-    {
-        int n = initial->getLength();
-
-        Square* rotated = new Square(n);
-
-        for(int row = 0; row < n; row++)
-            for(int col = 0; col < n; col++)
-                rotated->setPoint(col, (n-1) - row, initial->getPoint(row, col));
-
-        return rotated;
-    }
-
-    /* all possible transformations */
-    enum Transformations { CW_90_DEG, CW_180_DEG, CW_270_DEG, REFLECT, COMBO, NO_CHANGE, INVALID
-    };
-
-    Square(int sideLength) { 
-        n = sideLength; 
-        square = new bool[n * n]; 
-    }
-
-    Square(Square* s) {
-        n = s->getLength();
-        square = new bool[n * n];
-    }
-
-
-    /* 0-indexed*/
-    void setPoint(int i, int j, bool val) {
-        *(square + i * n + j) = val;
-    }
-
-    /* 0-indexed */
-    void setPoint(int i, int j, char val) {
-        *(square + i * n + j) = (val == '@');
-    }
-
-    /* 0-indexed */
-    bool getPoint(int i, int j) {
-        return *(square + i * n + j);
-    }
-
-    int getLength() {
-        return n;
-    }
-
-    void printSquare() {
-        for(int i = 0; i < n; i++)
+        /* Returns true if s1 and s2 have the same sidelength, false otherwise */
+        static bool verifySameDimensions(Square* s1, Square* s2)
         {
-            for(int j = 0; j < n; j++)
-                std::cout << toChar(getPoint(i, j));
-
-            std::cout << std::endl;
+            return s1->getLength() == s2->getLength();
         }
-    }
+
+        /* Returns a square equal to initial rotated 90 degrees clockwise */
+        static Square* rotate90DegCW(Square* initial)
+        {
+            int n = initial->getLength();
+
+            Square* rotated = new Square(n);
+
+            for(int row = 0; row < n; row++)
+                for(int col = 0; col < n; col++)
+                    rotated->setPoint(col, (n-1) - row, initial->getPoint(row, col));
+
+            return rotated;
+        }
+
+        /* all possible transformations */
+        enum Transformations { CW_90_DEG, CW_180_DEG, CW_270_DEG, REFLECT, COMBO, NO_CHANGE, INVALID
+        };
+
+        Square(int sideLength) { 
+            n = sideLength; 
+            square = new bool[n * n]; 
+        }
+
+        Square(Square* s) {
+            n = s->getLength();
+            square = new bool[n * n];
+        }
 
 
-private:
-    char toChar(bool val) {
-        return val ? '@' : '-';
-    } 
+        /* 0-indexed*/
+        void setPoint(int i, int j, bool val) {
+            *(square + i * n + j) = val;
+        }
 
-     /* The side length of the square */
-    int n;
-    /* The array representing the square */
-    bool* square;
+        /* 0-indexed */
+        void setPoint(int i, int j, char val) {
+            *(square + i * n + j) = (val == '@');
+        }
+
+        /* 0-indexed */
+        bool getPoint(int i, int j) {
+            return *(square + i * n + j);
+        }
+
+        int getLength() {
+            return n;
+        }
+
+        void printSquare() {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    std::cout << toChar(getPoint(i, j));
+
+                std::cout << std::endl;
+            }
+        }
+
+        bool operator==(Square& rhs)
+        {
+            Square lhs = this;
+            Square::verifySameDimensions(&lhs, &rhs);
+
+            int n = lhs.getLength();
+
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n; j++)
+                    if(lhs.getPoint(i, j) != rhs.getPoint(i, j))
+                        return false;
+
+            return true;
+        }
+    private:
+        char toChar(bool val) {
+            return val ? '@' : '-';
+        } 
+
+        /* The side length of the square */
+        int n;
+        /* The array representing the square */
+        bool* square;
 };
 
-bool operator==(Square& lhs, Square& rhs)
-{
-    Square::verifySameDimensions(&lhs, &rhs);
 
-    int n = lhs.getLength();
-
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            if(lhs.getPoint(i, j) != rhs.getPoint(i, j))
-                return false;
-
-    return true;
-}
 
 /**
  * Reads characters from input into square initial and after
@@ -111,9 +112,9 @@ static void Transform_readSquares(std::ifstream* input, Square* initial, Square*
     {
         for(int j = 0; j < n; j++)
         {
-           *input >> c;
+            *input >> c;
 
-           initial->setPoint(i, j, c);
+            initial->setPoint(i, j, c);
         }
     }
 
@@ -121,9 +122,9 @@ static void Transform_readSquares(std::ifstream* input, Square* initial, Square*
     {
         for(int j = 0; j < n; j++)
         {
-           *input >> c;
+            *input >> c;
 
-           after->setPoint(i, j, c);
+            after->setPoint(i, j, c);
         }
     }
 }
@@ -141,11 +142,12 @@ int main()
 
     Square* initial = new Square(n);
     Square* after = new Square(n);
-    
+
     /* Read in and verify squares output */
     Transform_readSquares(input, initial, after);
 
     initial->printSquare();
+    Square* test = initial;
     std::cout << "rotated" << std::endl;
     (Square::rotate90DegCW(initial))->printSquare();
     after->printSquare();
@@ -167,4 +169,4 @@ int main()
     delete(input);
 }
 
-    
+
